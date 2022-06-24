a3c178f63677594eee2d105ca2fed733c832328a
Claudio Jin <natsu165@hotmail.com>
2022-06-24 16:21:36 -0300

    Finalizado aula 04 - IgniteLab

--- event-platform/src/components/Router.tsx
+++ event-platform/src/components/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from "../pages/Event";
+import { Subscribe } from "../pages/Subscribe";
 //o dois pontos depois da barra representa uma informacao dinamica
 export function Router() {
     return (
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
+            <Route path="/" element={<Subscribe/>}/>
 
             <Route path="/event" element={<Event/>}/>
             <Route path="/event/lesson/:slug" element={<Event/>}/>
